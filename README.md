f727a1be6a7b5cc6c9565e12756074134a7c9f25
Tiago Brito <tfb@eurotux.com>
2017-03-30 11:29:49 +0100

    Prepared to package distribution

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # django-maillings
 Django module to easily send templated emails
+
+# Settings
+To give support to this app we need to declare de following django settings:
+
+.. code-block:: python
+	
+    DEFAULT_FROM_EMAIL="example@example.com",
+	MAILINGS={
+        'DEFAULT_REPLY_TO': "examplereplyto@example.com",
+        'DEFAULT_SUJECT': "Example subject",
+        'BASE_URL': "https://domain.com",
+        'EXTRA_DATA': {}
+    },
+
+- **DEFAULT_FROM_EMAIL**: Default setting of Django that defines the from email
+- **DEFAULT_REPLY_TO**: Default reply to be used on emails
+- **DEFAULT_SUJECT**: Default subject to be used on emails
+- **BASE_URL**: The base url of your website
+- **EXTRA_DATA**: Any extra data intended to be used on all emails (This is injected on context of template)
---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-mailings',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Django module to easily send templated emails.',
+    long_description=README,
+    url='https://github.com/dipcode-software/',
+    author='Dipcode',
+    author_email='info@dipcode.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
